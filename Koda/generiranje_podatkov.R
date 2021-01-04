1df7bfccc5a4216a2dcca285732dc2b4898903d1
ÄLara Vidmar <lara.snupi@gmail.com>
2021-01-04 20:33:11 +0100

    algoritem za odstranjevanje točk iz igrapha

--- Koda/generiranje_podatkov.R
+++ Koda/generiranje_podatkov.R
@@ -1,4 +1,6 @@
 #generianje podatkov in predstava v tabeli 
+library(ggplot2)
+
 
 #1) gledamo navadane grafe,ki jih generirava s pomočjo matrike 
 #a) vse povezave imajo enako vrednost
@@ -24,6 +26,7 @@ tabela_istih_utezi <- function(max_st_tock, max_utez){ #ta funkcija naredi tabel
 #primerček za zgornjo funkcijo 
 #tabela <- tabela_istih_utezi(5,5) 
 
+
 #opazimo nek algoritem 
 alg_iste_utezi <- function(st_tock, max_utez){
   zac_tock <- 2
@@ -38,21 +41,24 @@ alg_iste_utezi <- function(st_tock, max_utez){
     zac_tock <- zac_tock + 1
     vek <- c()
   }
-  colnames(tab_iste_utezi)[1] = "število točk"
+  colnames(tab_iste_utezi)[1] = "stevilo tock"
   colnames(tab_iste_utezi)[3] = "X2"
   return(tab_iste_utezi)
   
 }
+tabela_2 <- alg_iste_utezi(5,20)
+#ggplot(as.data.frame.matrix(tabela_2))??? 
+
 
 #b) utezi povezav so iz intervala [a,b]  a < b, in odstranjujemo povezave 
 #odstarnimo minimalno utež 
 
-tabela_nakljucne_utezi <- function(max_st_tock,a,b){
+tabela_odstrani_minpov <- function(max_st_tock,a,b){
   st_tock <- max_st_tock
   pretoki <- c()
   g <- pretvorba_v_igraph(generira_matriko(st_tock,a,b))
   pretoki <- append(pretoki, edmonds_karp(g,1,st_tock))
-  tab_odstrani_povezave <- data.frame()
+  tab_odstrani_min <- data.frame()
   dolzina <- 0
   while (st_tock > 3) {
     while (pretoki[length(pretoki)] != 0 ) {
@@ -60,27 +66,92 @@ tabela_nakljucne_utezi <- function(max_st_tock,a,b){
       mesto <- which(E(g)$V3== min)[1]
       g <- delete.edges(g,E(g)[mesto])
       pretoki <- append(pretoki, edmonds_karp(g, 1, st_tock))
-      print(pretoki)
     }
-    if (dolzina < length(pretoki)){
-      dolzina = length(pretoki)
-    } else {
-      dol <- length(pretoki)
-      pretoki <- append(pretoki, rep(0, dolzina-dol)) 
-    }
-    tab_odstrani_povezave <- rbind(tab_odstrani_povezave, pretoki)
+    if (length(pretoki) > dolzina){
+      dolzina <- length(pretoki)
+      
+    }else {
+      u <- length(pretoki)
+      pretoki <- c(pretoki, rep(0, dolzina - u))}
+    tab_odstrani_min <- rbind(tab_odstrani_min, pretoki)
+    dolzina <- dim(tab_odstrani_min)[2]
     st_tock <- st_tock - 1
     g <- pretvorba_v_igraph(generira_matriko(st_tock,a,b))
     pretoki <- c()
     pretoki <- append(pretoki, edmonds_karp(g,1,st_tock))
     
   }
-  return(tab_odstrani_povezave)
+  tab_odstrani_min <- cbind('stevilo tock' = c(4:max_st_tock), tab_odstrani_min)
+  colnames(tab_odstrani_min)[2:dolzina] <- c(1:(dolzina-1))
+  return(tab_odstrani_min)
 }
+#primer 
+#mini <-tabela_odstrani_minpov(10,0,20)
 
 #odstranimo maksimalno utez
 
+tabela_odstarni_maxpov <- function(max_st_tock,a,b){
+  st_tock <- max_st_tock
+  pretoki <- c()
+  g <- pretvorba_v_igraph(generira_matriko(st_tock,a,b))
+  pretoki <- append(pretoki, edmonds_karp(g,1,st_tock))
+  tab_odstrani_max <- data.frame()
+  dolzina <- 0
+  while (st_tock > 3) {
+    while (pretoki[length(pretoki)] != 0 ) {
+      max <- max(E(g)$V3)
+      mesto <- which(E(g)$V3== min)[1]
+      g <- delete.edges(g,E(g)[mesto])
+      pretoki <- append(pretoki, edmonds_karp(g, 1, st_tock))
+    }
+    if (length(pretoki) > dolzina){
+      dolzina <- length(pretoki)
+      
+    }else {
+      u <- length(pretoki)
+      pretoki <- c(pretoki, rep(0, dolzina - u))}
+    tab_odstrani_max <- rbind(tab_odstrani_max, pretoki)
+    dolzina <- dim(tab_odstrani_max)[2]
+    st_tock <- st_tock - 1
+    g <- pretvorba_v_igraph(generira_matriko(st_tock,a,b))
+    pretoki <- c()
+    pretoki <- append(pretoki, edmonds_karp(g,1,st_tock))
+    
+  }
+  tab_odstrani_max <- cbind('stevilo tock' = c(4:max_st_tock), tab_odstrani_max)
+  colnames(tab_odstrani_max)[2:dolzina] <- c(1:(dolzina-1))
+  return(tab_odstrani_max)
+}
+
+#primer 
+#maxi <-tabela_odstarni_maxpov(10,0,20)
+
 #c) utezi povezav so iz intervala [a,b]  a < b, in odstranjujemo točke
+tabela_odstrani_tocke <- function(max_tock, a, b){
+  st_tock <- max_tock
+  pretoki <- c()
+  g <- pretvorba_v_igraph(generira_matriko(st_tock,a,b))
+  pretoki <- append(pretoki, edmonds_karp(g,1,st_tock))
+  tab_odstrani_oglisce <- data.frame()
+  dolzina <- 0
+  while (st_tock > 3) {
+      for (i in 1: (st_tock-2)){
+        k <- sample(2:(st_tock-1), size = i, replace=FALSE)
+        h <- delete.vertices(g,k)
+        pretoki <- append(pretoki, edmonds_karp(h,1,st_tock-i))
+    
+      }
+    pretoki <- append(pretoki, rep(0,dolzina))
+    tab_odstrani_oglisce <- rbind(tab_odstrani_oglisce, pretoki)
+    st_tock <- st_tock -1 
+    dolzina <- dolzina +1
+    g <- pretvorba_v_igraph(generira_matriko(st_tock,a,b))
+    pretoki <- c()
+    pretoki <- append(pretoki, edmonds_karp(g,1,st_tock))
+  }
+  return(tab_odstrani_oglisce)
+}
+
 
 
 #2) generiramo s pomočjo geometrijskih grafov 
